b9e31f9344e13e09692956e9438bc252bb9d1b24
Guilherme Saad <105023562+guilhermesaad@users.noreply.github.com>
2023-03-20 14:41:03 -0300

    Implementação da estrutura pilha.
    Exercício dado em sala para implementar a estrutura pilha (usando array).

---
+++ exercicioProva_ED2/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioProva_ED2
+{
+    class Pilha
+    {
+        // elementos - atributos
+
+        private char[] pilhaChar = new char[20];
+        private int topo;
+
+        // operadores - métodos
+
+        // construtor
+        public Pilha()
+        {
+            topo = -1;
+        }
+
+        public bool estaVazia()
+        {
+            if (topo == -1)
+                return true;
+            else
+                return false;
+        }
+
+        public void insere(char c) // push
+        {
+            topo++;
+            pilhaChar[topo] = c;
+        }
+
+        public char remove() // pop
+        {
+            char temp = pilhaChar[topo];
+            topo--;
+            return temp;
+
+        }
+
+        public void imprime()
+        {
+            int temp = topo;
+            if (estaVazia())
+                Console.WriteLine("A Pilha esta vazia");
+            else
+            {
+                while (temp != -1)
+                {
+                    Console.Write(pilhaChar[temp]);
+                    //Console.WriteLine();
+                    temp--;
+                }
+            }
+        }
+
+        public char top()
+        {
+            if(topo == -1)
+                return '!';
+            return pilhaChar[topo];
+        }
+
+
+    } // fim da classe Pilha
+}
+
---
+++ exercicioProva_ED2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioProva_ED2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua string na forma 'x C y'");
+            string entrada = Console.ReadLine();
+
+            int i = 0;
+            int j = -1;
+            int tamX = 0;
+            int tamY = 0;
+
+            for (; entrada[i] != 'C'; i++)
+            { }
+            tamX = i;
+            for (; i < entrada.Length; i++)
+            { j++; }
+            tamY = j;
+
+            //Console.WriteLine(tamX);
+            //Console.WriteLine(tamY);
+
+            if (tamX != tamY)
+                Console.WriteLine("Errado ");
+            else
+            {
+                char[] vecX = new char[tamX];
+                char[] vecY = new char[tamY];
+                Pilha pilhaA = new Pilha();
+                Pilha pilhaB = new Pilha();
+
+                
+                for (i = 0; entrada[i] != 'C'; i++)
+                {
+                    vecX[i] = entrada[i];
+                }
+                    
+                tamX = i;
+                for (j = 0; i+1 < entrada.Length; i++, j++)
+                {
+                    vecY[j] = entrada[i+1];
+                }
+                    
+
+                
+                for (i = 0; i < tamX; i++)
+                {
+
+                    if (vecX[i] == 'A')  // Mexe com o X
+                    {
+                        if (pilhaA.top() == 'A')
+                            pilhaA.remove();
+                        else
+                        pilhaA.insere('A');
+                       
+                    }
+
+                    if (vecX[i] == 'B')
+                    {
+                        if (pilhaB.top() == 'B')
+                            pilhaB.remove();
+                        else
+                        pilhaB.insere('B');
+
+                    }
+
+                    if (vecY[i] == 'A')  // Mexe com o Y
+                    {
+                        if (pilhaA.top() == 'A')
+                            pilhaA.remove();
+                        else
+                            pilhaA.insere('A');
+
+                    }
+
+                    if (vecY[i] == 'B')
+                    {
+                        if (pilhaB.top() == 'B')
+                            pilhaB.remove();
+                        else
+                            pilhaB.insere('B');
+                        
+                    }
+
+
+                }
+                if (pilhaA.estaVazia() && pilhaB.estaVazia())
+                    Console.WriteLine("Correto ");
+                else
+                    Console.WriteLine("Errado");
+                
+            }
+        }
+    }
+}
